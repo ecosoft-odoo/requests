ac65780f3a5e55f3e38322be24ec887e360d8134
Kitti U <kittiu@gmail.com>
2021-02-04 08:03:54 +0700

    Add test for requests_spending_multi

--- requests_spending_multi/tests/test_requests_spending_multi.py
+++ requests_spending_multi/tests/test_requests_spending_multi.py
@@ -4,14 +4,24 @@
 from odoo.tests.common import Form, SavepointCase
 
 
-class TestRequestsHRAdvance(SavepointCase):
+class TestRequestsSpendingMulti(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.category = cls.env.ref(
-            "requests_hr_advance.request_category_data_hr_advance"
+            "requests_spending_multi.request_category_data_spending_multi"
         )
         cls.approver = cls.env.ref("base.user_admin")
+        cls.product_mouse = cls.env["product.product"].create(
+            {
+                "name": "Computer Mouse",
+            }
+        )
+        cls.product_laptop = cls.env["product.product"].create(
+            {
+                "name": "Laptop",
+            }
+        )
         advance_account = cls.env["account.account"].create(
             {
                 "code": "154000",
@@ -43,14 +53,38 @@ class TestRequestsHRAdvance(SavepointCase):
             req_approver.user_id = approver
         return create_request_form
 
-    def test_01_hr_advance(self):
-        # Create new advance request and create advance.
+    def test_01_spending_multi(self):
+        # Create new spending request request and create PR/AV/EX.
         request_form = self.create_request_form(self.approver, self.category)
-        request_form.amount = 1000
+        request_form.amount = 1000  # AV
+        with request_form.product_line_ids.new() as line:
+            line.product_id = self.product_mouse
+            line.quantity = 1
+            line.price_unit = 100
+            line.res_type = "ex"  # EX
+        with request_form.product_line_ids.new() as line:
+            line.product_id = self.product_mouse
+            line.quantity = 2
+            line.price_unit = 1000
+            line.res_type = "pr"  # PR
         request = request_form.save()
         request.action_confirm()
         request.with_user(self.approver).action_approve()
-        # Now, a AV should have been created
+        # Now, all PR/AV/EX should have been created
+        # PR
+        self.assertEqual(request.purchase_request_count, 1)
+        res = request.action_open_purchase_request()
+        pr_id = res["domain"][0][2]
+        purchase_request = self.env["purchase.request"].browse(pr_id)
+        self.assertEqual(len(purchase_request.line_ids), 1)
+        # EX
+        self.assertEqual(request.hr_expense_count, 1)
+        res = request.action_open_hr_expense()
+        ex_id = res["domain"][0][2]
+        expense = self.env["hr.expense.sheet"].browse(ex_id)
+        self.assertEqual(len(expense.expense_line_ids), 1)
+        # AV
+        self.assertEqual(request.hr_advance_count, 1)
         self.assertEqual(request.hr_advance_count, 1)
         res = request.action_open_hr_advance()
         av_id = res["domain"][0][2]
